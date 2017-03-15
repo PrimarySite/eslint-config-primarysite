ded6e28796464e1074ae377d816581e807492ef6
SammyM <sammy.marsden@primarysite.net>
2017-03-15 09:58:45 +0000

    Added some node rules

---
+++ rules/node.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    'no-mixed-requires': 'error',
+    'no-new-require': 'error',
+    'no-path-concat': 'error',
+    'no-process-env': 'error',
+    'no-process-exit': 'error',
+  }
+};
