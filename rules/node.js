7c4498221b7c2750bbf27bd7b681108f98cccac0
Kristian Roebuck <roebuck86@gmail.com>
2017-03-27 14:56:02 +0100

    Prevent the prune

--- rules/node.js
+++ rules/node.js
@@ -1,9 +1,12 @@
 module.exports = {
+  env: {
+    node: true,
+  },
   rules: {
     'no-mixed-requires': 'error',
     'no-new-require': 'error',
     'no-path-concat': 'error',
     'no-process-env': 'error',
     'no-process-exit': 'error',
-  }
+  },
 };
