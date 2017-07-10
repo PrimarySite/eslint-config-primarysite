40305a54a3edcf2afc5422ce946221f4ee39cb88
SammyM <sammy.marsden@primarysite.net>
2017-07-10 14:14:38 +0100

    Added support for prettier

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 module.exports = {
   extends: [
-    './rules/best-practices',
-    './rules/errors',
-    './rules/es2015',
-    './rules/imports',
-    './rules/node',
-    './rules/react',
-    './rules/react-a11y',
-    './rules/style',
-    './rules/tests',
-    './rules/variables'
-  ].map(require.resolve)
+    [
+      './rules/best-practices',
+      './rules/errors',
+      './rules/es2015',
+      './rules/imports',
+      './rules/node',
+      './rules/react',
+      './rules/react-a11y',
+      './rules/style',
+      './rules/tests',
+      './rules/variables'
+    ].map(require.resolve),
+    'prettier',
+    'prettier/react'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        jsxBracketSameLine: true,
+        printWidth: 99,
+        singleQuote: true
+      }
+    ]
+  }
 };
