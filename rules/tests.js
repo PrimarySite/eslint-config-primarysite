c9be96f1e89ccdd60b5b70d94329754d62c6a620
TheGallery <iopsychas@gmail.com>
2019-10-30 10:56:37 +0000

    Add Cypress linting configuration

--- rules/tests.js
+++ rules/tests.js
@@ -1,5 +1,12 @@
 module.exports = {
   env: {
+    'cypress/globals': true,
     jest: true
   },
+  plugins: ['cypress'],
+  rules: {
+    'cypress/no-assigning-return-values': 'error',
+    'cypress/no-unnecessary-waiting': 'error',
+    'cypress/assertion-before-screenshot': 'error'
+  }
 };
