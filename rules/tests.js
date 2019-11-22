2b87986248d2a5440431398b3a8e53b5ea25b4fb
TheGallery <iopsychas@gmail.com>
2019-11-22 08:48:23 +0000

    Alphabetise object keys

--- rules/tests.js
+++ rules/tests.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   plugins: ['cypress'],
   rules: {
+    'cypress/assertion-before-screenshot': 'error',
     'cypress/no-assigning-return-values': 'error',
     'cypress/no-unnecessary-waiting': 'error',
-    'cypress/assertion-before-screenshot': 'error'
   }
 };
